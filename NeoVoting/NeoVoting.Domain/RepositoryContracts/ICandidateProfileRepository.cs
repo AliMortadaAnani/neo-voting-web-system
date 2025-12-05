fbb1dd0abfd4bf6fde5557beb7989ded99ffbb96
Ali Mortada Anani <alimortadaanani@gmail.com>
2025-12-05 13:36:43 +0200

    Version 2.7.0 - NeoVoting System Re-Setup

--- NeoVoting/NeoVoting.Domain/RepositoryContracts/ICandidateProfileRepository.cs
+++ NeoVoting/NeoVoting.Domain/RepositoryContracts/ICandidateProfileRepository.cs
@@ -4,7 +4,7 @@ namespace NeoVoting.Domain.RepositoryContracts
 {
     public interface ICandidateProfileRepository
     {
-
+        //test git
         Task<List<CandidateProfile>> GetAllCandidatesProfilesAsync(CancellationToken cancellationToken);
 
         Task<CandidateProfile?> GetCandidateProfileByIdAsync(Guid id, CancellationToken cancellationToken);
